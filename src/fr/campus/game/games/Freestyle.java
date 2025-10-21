6fad667cd75d6ac99af2d87f1aa1c94761c6f048
Paul Montoussy <paul.montoussy@gmail.com>
2025-10-21 14:46:29 +0200

    we are seeing le bout du tunnel

--- src/fr/campus/game/games/Freestyle.java
+++ src/fr/campus/game/games/Freestyle.java
@@ -1,6 +1,7 @@
 package fr.campus.game.games;
 
 import fr.campus.game.board.Board;
+import fr.campus.main.Menu;
 import fr.campus.support.UserInteraction;
 import fr.campus.support.View;
 import fr.campus.support.player.Player;
@@ -15,6 +16,9 @@ public class Freestyle extends GameType{
     @Override
     public void init()
     {
+        menu = new Menu();
+        this.players = menu.displayPlayerChoiceMenu();
+
         this.columnMax =  UserInteraction.askForInt("How many columns in your game? (choose between 1 and " + MAX_SIZE + "): ", 1, MAX_SIZE);
         this.lineMax = UserInteraction.askForInt("How many rows in your game? (choose between 1 and " + MAX_SIZE + "): ", 1, MAX_SIZE);
         this.winRule =  UserInteraction.askForInt("How many cells in a row to win the game?", 1, MAX_SIZE);
